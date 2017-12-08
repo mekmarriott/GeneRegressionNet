c682ce3896181ee622e78d45999a0bbc1ee935e1
root <root@group18.0ba1f3wsyyiernwly4ecs542ge.jx.internal.cloudapp.net>
2017-12-08 08:18:54 +0000

    added cross-validation fold testing

--- dnn_embed_test.py
+++ dnn_embed_test.py
@@ -17,6 +17,7 @@ ACTIVATION = 'relu'
 OPTIMIZATION = 'adam'
 COMBINER = 'mean'
 SURVIVAL = 'cox'
+CV_FOLD = 4
 
 if __name__ == "__main__":
 
@@ -38,29 +39,43 @@ if __name__ == "__main__":
     for key in dataset:
       print(key, dataset[key].shape)
     print("Embedding is size: ", E.shape)
-    train_feed = {
+    data_feed = {
       key.split('_')[0] : dataset[key]
       for key in dataset if 'train' in key
     }
-    test_feed = {
+    validation_feed = {
       key.split('_')[0] : dataset[key]
       for key in dataset if 'test' in key
     }
 
-    train_feed['embed'] = E
-    test_feed['embed'] = E
-    print("*"*40)
+    datasets = training_utils.cv_split(data_feed, partitions=CV_FOLD, sparse_keys=['x'])
+    print("CV Dataset contains the following:")
+    for key in dataset:
+      print(key, dataset[key].shape)
 
-    print("Testing custom loss on DNN Embedding Tensorflow Model using %s" % EMBEDDING)
-    loss_history = []
-    m = TFDNNEmbeddingModel(embed_shape, alpha=ALPHAS[cancer], combiner=COMBINER, layer_dims=LAYERS, loss=SURVIVAL, activation=ACTIVATION, opt=OPTIMIZATION, output_sz=time_buckets)
-    m.initialize()
-    for i in range(int(NUM_ITERATIONS/100)):
-      m.train(train_feed, num_iterations=100, debug=False)
-      embed_train_loss = m.test(train_feed)
-      embed_test_loss = m.test(test_feed)
-      loss_history.append(np.array([i,embed_train_loss, embed_test_loss]))
-      print("DNN Embedding Model train loss is %.6f and test loss is %.6f" % (embed_train_loss, embed_test_loss))
+    for d in datasets:
+      train_feed = {
+        key.split('_')[0] : dataset[key]
+        for key in dataset if 'train' in key
+      }
+      test_feed = {
+        key.split('_')[0] : dataset[key]
+        for key in dataset if 'test' in key
+      }
+      train_feed['embed'] = E
+      test_feed['embed'] = E
+      print("*"*40)
 
-    # Save loss history data under descriptive name
-    np.save('%s/%s/%s_layers-%s_%s_%s_%s_it-%d_alpha-%s' % (RUN_DIR, cancer, EMBEDDING, '-'.join([str(l) for l in LAYERS]), ACTIVATION, OPTIMIZATION, COMBINER, NUM_ITERATIONS, str(ALPHAS[cancer])), loss_history)
+      print("Testing custom loss on DNN Embedding Tensorflow Model using %s" % EMBEDDING)
+      loss_history = []
+      m = TFDNNEmbeddingModel(embed_shape, alpha=ALPHAS[cancer], combiner=COMBINER, layer_dims=LAYERS, loss=SURVIVAL, activation=ACTIVATION, opt=OPTIMIZATION, output_sz=time_buckets)
+      m.initialize()
+      for i in range(int(NUM_ITERATIONS/100)):
+        m.train(train_feed, num_iterations=100, debug=False)
+        embed_train_loss = m.test(train_feed)
+        embed_test_loss = m.test(test_feed)
+        loss_history.append(np.array([i,embed_train_loss, embed_test_loss]))
+        print("DNN Embedding Model train loss is %.6f and test loss is %.6f" % (embed_train_loss, embed_test_loss))
+
+      # Save loss history data under descriptive name
+      np.save('%s/%s/%s_layers-%s_%s_%s_%s_it-%d_alpha-%s' % (RUN_DIR, cancer, EMBEDDING, '-'.join([str(l) for l in LAYERS]), ACTIVATION, OPTIMIZATION, COMBINER, NUM_ITERATIONS, str(ALPHAS[cancer])), loss_history)
--- training_utils.py
+++ training_utils.py
@@ -1,7 +1,11 @@
 import numpy as np
 from collections import defaultdict
 
-def sparse_reorder_split(m, new_order, split_idx):
+def sparse_reorder_split(m, new_order, split_range):
+  assert len(split_range) == 2, "Length of split range must be 2"
+  assert split_range[0] >= 0 and split_range[1] >= 0, "Split range must have values > 0"
+  assert split_range[0] <= len(new_order) and split_range[1] <= len(new_order), "Split range must be less than/equal length of array"
+  assert split_range[0] <= split_range[1], "Max split range %d is less than min split range %d!" % tuple(split_range)
   assert m.shape[1] == 2, "Shape of sparse input must be (?, 2)"
   idx_to_values = defaultdict(list)
   train_indices, train_values, test_indices, test_values = [], [], [], []
@@ -15,11 +19,14 @@ def sparse_reorder_split(m, new_order, split_idx):
     if len(values) == 0:
       print("There is no data for patient at index %d" % new_order[i])
       continue
-    if i < split_idx:
-      train_indices.extend([np.array([i])]*len(values))
+    if i >= split_range[0] and i < split_range[1]:
+      train_indices.extend([np.array([i-split_range[0]])]*len(values))
       train_values.extend(values)
     else:
-      test_indices.extend([np.array([i-split_idx])]*len(values))
+      mod_i = i
+      if i >= split_range[1]:
+        mod_i -= split_range[1] 
+      test_indices.extend([np.array([mod_i])]*len(values))
       test_values.extend(values)
   return np.array(train_indices), np.array(train_values), np.array(test_indices), np.array(test_values)
 
@@ -41,7 +48,7 @@ def train_test_split(d, split=0.7, sparse_keys=[]):
             if N > 0:
                 assert N == d[key].shape[0], "Dataset under key %s has length %d, expected length %d" % (key, d[key].shape[0], N)
             else:
-                N = d[key].shape[0]
+               N = d[key].shape[0]
     indices = np.random.permutation(N)
     split_idx = int(N*split)
     assert split_idx > 0 and split_idx < N, "Split index cannot leave empty training or test set - split index is %d, split is %f, and the size to split is %d" % (split_idx, split, N)
@@ -49,13 +56,44 @@ def train_test_split(d, split=0.7, sparse_keys=[]):
     for key in d:
       x = d[key]
       if key in sparse_keys:
-        result[key + "-indices_train"], result[key + "-values_train"], result[key + "-indices_test"], result[key + "-values_test"] = sparse_reorder_split(x, indices, split_idx)
+        result[key + "-indices_train"], result[key + "-values_train"], result[key + "-indices_test"], result[key + "-values_test"] = sparse_reorder_split(x, indices, [0,split_idx])
         result[key + "-shape_train"] = np.array(result[key + "-values_train"].shape)
         result[key + "-shape_test"] = np.array(result[key + "-values_test"].shape)
         continue
       assert len(x) == N, "Dataset under key %s has shape %s, expected first dimension of %d" % (key, str(x.shape), N)
       result[key + "_train"] = x[indices[:split_idx]]
       result[key + "_test"] = x[indices[split_idx:]]
+    return result
+
+def cv_split(d, partitions=5, sparse_keys=[]):
+    assert len(d) > 0, "Must have at least one key/entry for the cross validation dataset"
+    for key in sparse_keys:
+        assert key + "-indices" in d and key + "-values" in d and key + "-shape" in d, "Need both indices and shape for key %s in dataset!" % key
+        d[key] = np.vstack([d[key + "-indices"].flatten(), d[key + "-values"]]).T
+        print("new shape is", d[key].shape)
+        del d[key + "-indices"]
+        del d[key + "-shape"]
+        del d[key + "-values"]
+    for key in d:
+        assert type(d[key]).__module__ == np.__name__, "Must pass in a numpy array type!"
+        if key not in sparse_keys:
+            N = d[key].shape[0]
+            break
+    assert N > 0, "Must have at least one key in the dataset with non zero, non sparse data"
+    assert N > partitions, "Must have at least as many elements in dataset as partitions"
+    indices = np.random.permutation(N)
+    split_indices = [int(N*i/(partitions-1.0)) for i in range(partitions)]
+    split_indices = [0] + split_indices + [N]
+    result = []
+    for partition_idx in range(partitions):
+        _d = {}
+        for key in d:
+            if key in sparse_keys:
+                _d[key + "-indices_train"], _d[key + "-values_train"], _d[key + "-indices_test"], _d[key + "-values_test"] = sparse_reorder_split(d[key], indices, [split_indices[partition_idx], split_indices[partition_idx+1]])
+                _d[key + "-shape_train"], _d[key + "-shape_test"] = np.array(_d[key + "-values_train"].shape), np.array(_d[key + "-values_test"].shape)
+            else:
+                _d[key + "_train"], _d[key + "_test"] = d[key][indices[split_indices[partition_idx]:split_indices[partition_idx+1]]], np.append(d[key][indices[:split_indices[partition_idx]]], d[key][indices[split_indices[partition_idx+1]:]])
+        result.append(_d)                
     return result
 
 def feature_normalize(dataset):
